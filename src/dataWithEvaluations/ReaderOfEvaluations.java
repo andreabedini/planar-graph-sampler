4b07878c752a34ea351eb04674cb1dc77886eb1d
Andrea Bedini <abedini@ms.unimelb.edu.au>
2010-10-27 15:27:33 +1100

    Reformat

--- src/dataWithEvaluations/ReaderOfEvaluations.java
+++ src/dataWithEvaluations/ReaderOfEvaluations.java
@@ -9,108 +9,202 @@ import java.io.FileNotFoundException;
 import maps.*;
 
 public class ReaderOfEvaluations {
-    public static String FILE_NETWORKS="../../src/dataWithEvaluations/values_networks";
-    public static String FILE_PLANAR_GRAPHS="../../src/dataWithEvaluations/values_planar";
 
-    /** Creates a new instance of ReaderOfEvaluations */
-    public ReaderOfEvaluations() {
+    public static String FILE_NETWORKS = "../../src/dataWithEvaluations/values_networks";
+    public static String FILE_TWO_CONNECTED = "../../src/dataWithEvaluations/values_planar";
+    public static String FILE_PLANAR_GRAPHS = "../../src/dataWithEvaluations/values_planar";
+
+    public static void loadNetworkGeneratingFunctionsValues(String size) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(FILE_NETWORKS));
+            findSize(size, reader);
+            readBigNumberBinaryTrees(reader);
+            readBigNumber3connected(reader);
+            readBigNumberNetworks(reader);
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
     }
 
-    public static void loadNetworkGeneratingFunctionsValues(BufferedReader reader) {
-        readBigNumberBinaryTrees(reader);
-        readBigNumber3connected(reader);
-        readBigNumberNetworks(reader);
+    public static void loadTwoConnectedValues(String size) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(FILE_TWO_CONNECTED));
+            findSize(size, reader);
+            readBigNumberBinaryTrees(reader);
+            readBigNumber3connected(reader);
+            readBigNumberNetworks(reader);
+            readBigNumber2Connected(reader);
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
     }
 
-    public static void loadAllGeneratingFunctionsValues(BufferedReader reader) {
-        readBigNumberBinaryTrees(reader);
-        readBigNumber3connected(reader);
-        readBigNumberNetworks(reader);
-        readBigNumber2Connected(reader);
-        readBigNumberConnected(reader);
-        readBigNumberPlanar(reader);
+    public static void loadAllGeneratingFunctionsValues(String size) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(FILE_PLANAR_GRAPHS));
+            findSize(size, reader);
+            readBigNumberBinaryTrees(reader);
+            readBigNumber3connected(reader);
+            readBigNumberNetworks(reader);
+            readBigNumber2Connected(reader);
+            readBigNumberConnected(reader);
+            readBigNumberPlanar(reader);
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
     }
 
-    public static void readBigNumber3connected(BufferedReader reader){
-        affectChooseVector(ThreeConnectedNetwork.ch_K_in_dyK,reader);
-        affectChooseVector(ThreeConnectedNetwork.ch_dxK_in_dxyK,reader);
-        affectChooseVector(ThreeConnectedNetwork.ch_b_or_dxb,reader);
-        affectChooseVector(ThreeConnectedNetwork.ch_3b_or_dyb,reader);
+    public static void readBigNumber3connected(BufferedReader reader) {
+        System.err.println("Loading 4 vectors for 3 connected");
+        affectChooseVector(ThreeConnectedNetwork.ch_K_in_dyK, reader);
+        System.err.println("ch_K_in_dyK loaded");
+        affectChooseVector(ThreeConnectedNetwork.ch_dxK_in_dxyK, reader);
+        System.err.println("ch_dxK_in_dxyK loaded");
+        affectChooseVector(ThreeConnectedNetwork.ch_b_or_dxb, reader);
+        System.err.println("ch_b_or_dxb loaded");
+        affectChooseVector(ThreeConnectedNetwork.ch_3b_or_dyb, reader);
+        System.err.println("ch_3b_or_dyb loaded");
     }
 
-    public static void readBigNumber2Connected(BufferedReader reader){
-        affectChooseVector(TwoConnectedMap.ch_xy_in_dB,reader);
-        affectChooseVector(TwoConnectedMap.ch_y_in_ddB,reader);
-        affectChooseVector(TwoConnectedMap.ch_nontrivialD_or_dD,reader);
-        affectChooseVector(TwoConnectedMap.ch_dD_or_ddD,reader);
+    public static void readBigNumber2Connected(BufferedReader reader) {
+        System.err.println("Loading 4 vectors for 2 connected");
+        affectChooseVector(TwoConnectedMap.ch_xy_in_dB, reader);
+        System.err.println("ch_K_in_dyK loaded");
+        affectChooseVector(TwoConnectedMap.ch_y_in_ddB, reader);
+        System.err.println("ch_K_in_dyK loaded");
+        affectChooseVector(TwoConnectedMap.ch_nontrivialD_or_dD, reader);
+        System.err.println("ch_K_in_dyK loaded");
+        affectChooseVector(TwoConnectedMap.ch_dD_or_ddD, reader);
+        System.err.println("ch_K_in_dyK loaded");
     }
 
-    public static void readBigNumberBinaryTrees(BufferedReader reader){
-        affectChooseVector(BinaryTree.ch_1_or_u,reader);
-        affectChooseVector(BinaryTree.ch_1_or_v,reader);
-        affectChooseVector(BinaryTree.ch_u_or_v,reader);
-        affectChooseVector(BinaryTree.ch_dxu_or_dxv,reader);
-        affectChooseVector(BinaryTree.choose_vector_dxu,reader);
-        affectChooseVector(BinaryTree.choose_vector_dxv,reader);
-        affectChooseVector(BinaryTree.ch_dyu_or_dyv,reader);
-        affectChooseVector(BinaryTree.choose_vector_dyv,reader);
-        affectChooseVector(BinaryTree.choose_vector_dyu,reader);
+    public static void readBigNumberBinaryTrees(BufferedReader reader) {
+        System.err.println("Loading 9 vectors for binary trees");
+        affectChooseVector(BinaryTree.ch_1_or_u, reader);
+        System.err.println("ch_1_or_u loaded");
+        affectChooseVector(BinaryTree.ch_1_or_v, reader);
+        System.err.println("ch_1_or_v loaded");
+        affectChooseVector(BinaryTree.ch_u_or_v, reader);
+        System.err.println("ch_u_or_v loaded");
+        affectChooseVector(BinaryTree.ch_dxu_or_dxv, reader);
+        System.err.println("ch_dxu_or_dxv loaded");
+        affectChooseVector(BinaryTree.choose_vector_dxu, reader);
+        System.err.println("choose_vector_dxu loaded");
+        affectChooseVector(BinaryTree.choose_vector_dxv, reader);
+        System.err.println("choose_vector_dxv loaded");
+        affectChooseVector(BinaryTree.ch_dyu_or_dyv, reader);
+        System.err.println("ch_dyu_or_dyv loaded");
+        affectChooseVector(BinaryTree.choose_vector_dyv, reader);
+        System.err.println("choose_vector_dyv loaded");
+        affectChooseVector(BinaryTree.choose_vector_dyu, reader);
+        System.err.println("choose_vector_dyu loaded");
     }
 
-    public static void readBigNumberConnected(BufferedReader reader){
-        affectChooseVector(ConnectedMap.poisson_dB,reader);
-        affectChooseVector(ConnectedMap.ch_dC_or_ddC,reader);
-        affectChooseVector(ConnectedMap.ch_2ddC_or_dddC,reader);
-        affectChooseVector(ConnectedMap.choose_vector_dddC,reader);
+    public static void readBigNumberConnected(BufferedReader reader) {
+        System.err.println("Loading 4 vectors for connected maps");
+        affectChooseVector(ConnectedMap.poisson_dB, reader);
+        System.err.println("poisson_dB loaded");
+        affectChooseVector(ConnectedMap.ch_dC_or_ddC, reader);
+        System.err.println("ch_dC_or_ddC loaded");
+        affectChooseVector(ConnectedMap.ch_2ddC_or_dddC, reader);
+        System.err.println("ch_2ddC_or_dddC loaded");
+        affectChooseVector(ConnectedMap.choose_vector_dddC, reader);
+        System.err.println("choose_vector_dddC loaded");
     }
 
-    public static void readBigNumberNetworks(BufferedReader reader){
-
-         affectChooseVector(Network.choose_vector_non_trivial_D,reader);
-         affectChooseVector(Network.choose_vector_D,reader);
-         affectChooseVector(Network.choose_vector_P,reader);
-         affectChooseVector(Network.ch_y_or_P_or_H,reader);
-         affectChooseVector(Network.ch_S_or_H,reader);
-         affectChooseVector(Network.poisson_S_plus_H,reader);
-         affectChooseVector(Network.poisson_at_least1_S_plus_H,reader);
-         affectChooseVector(Network.poisson_at_least2_S_plus_H,reader);
-         affectChooseVector(Network.choose_vector_dD,reader);
-         affectChooseVector(Network.choose_vector_dS,reader);
-         affectChooseVector(Network.choose_vector_dP,reader);
-         affectChooseVector(Network.choose_vector_dH,reader);
-         affectChooseVector(Network.ch_dP_or_dH,reader);
-         affectChooseVector(Network.ch_dS_or_dH,reader);
-         affectChooseVector(Network.choose_vector_ddD,reader);
-         affectChooseVector(Network.choose_vector_ddS,reader);
-         affectChooseVector(Network.choose_vector_ddP,reader);
-         affectChooseVector(Network.choose_vector_ddH,reader);
-         affectChooseVector(Network.ch_ddP_or_ddH,reader);
-         affectChooseVector(Network.ch_ddS_or_ddH,reader);
+    public static void readBigNumberNetworks(BufferedReader reader) {
+        System.err.println("Loading 21 vectors for networks");
+        affectChooseVector(Network.choose_vector_non_trivial_D, reader);
+        System.err.println("choose_vector_non_trivial_D loaded");
+        affectChooseVector(Network.choose_vector_D, reader);
+        System.err.println("choose_vector_D loaded");
+        affectChooseVector(Network.choose_vector_P, reader);
+        System.err.println("choose_vector_P loaded");
+        affectChooseVector(Network.ch_y_or_P_or_H, reader);
+        System.err.println("ch_y_or_P_or_H loaded");
+        affectChooseVector(Network.ch_S_or_H, reader);
+        System.err.println("ch_S_or_H loaded");
+        affectChooseVector(Network.poisson_S_plus_H, reader);
+        System.err.println("poisson_S_plus_H loaded");
+        affectChooseVector(Network.poisson_at_least1_S_plus_H, reader);
+        System.err.println("poisson_at_least1_S_plus_H loaded");
+        affectChooseVector(Network.poisson_at_least2_S_plus_H, reader);
+        System.err.println("poisson_at_least2_S_plus_H loaded");
+        affectChooseVector(Network.choose_vector_dD, reader);
+        System.err.println("choose_vector_dD loaded");
+        affectChooseVector(Network.choose_vector_dS, reader);
+        System.err.println("choose_vector_dS loaded");
+        affectChooseVector(Network.choose_vector_dP, reader);
+        System.err.println("choose_vector_dP loaded");
+        affectChooseVector(Network.choose_vector_dH, reader);
+        System.err.println("choose_vector_dH loaded");
+        affectChooseVector(Network.ch_dP_or_dH, reader);
+        System.err.println("ch_dP_or_dH loaded");
+        affectChooseVector(Network.ch_dS_or_dH, reader);
+        System.err.println("ch_dS_or_dH loaded");
+        affectChooseVector(Network.choose_vector_ddD, reader);
+        System.err.println("choose_vector_ddD loaded");
+        affectChooseVector(Network.choose_vector_ddS, reader);
+        System.err.println("choose_vector_ddS loaded");
+        affectChooseVector(Network.choose_vector_ddP, reader);
+        System.err.println("choose_vector_ddP loaded");
+        affectChooseVector(Network.choose_vector_ddH, reader);
+        System.err.println("choose_vector_ddH loaded");
+        affectChooseVector(Network.ch_ddP_or_ddH, reader);
+        System.err.println("ch_ddP_or_ddH loaded");
+        affectChooseVector(Network.ch_ddS_or_ddH, reader);
+        System.err.println("ch_ddS_or_ddH loaded");
     }
 
-    public static void readBigNumberPlanar(BufferedReader reader){
-        affectChooseVector(PlanarMap.poisson_C,reader);
-        affectChooseVector(PlanarMap.choose_vector_ddG,reader);
-        affectChooseVector(PlanarMap.choose_vector_dddG,reader);
+    public static void readBigNumberPlanar(BufferedReader reader) {
+        System.err.println("Loading 3 vectors for planar map");
+        affectChooseVector(PlanarMap.poisson_C, reader);
+        System.err.println("poisson_C loaded");
+        affectChooseVector(PlanarMap.choose_vector_ddG, reader);
+        System.err.println("choose_vector_ddG loaded");
+        affectChooseVector(PlanarMap.choose_vector_dddG, reader);
+        System.err.println("choose_vector_dddG loaded");
     }
 
-    public static void affectChooseVector(ChooseVector vector, BufferedReader reader){
-        int i=0; String s="";
-        try{
-            s=reader.readLine();
-            s=reader.readLine();// System.out.println(s);
-            StringTokenizer st=new StringTokenizer(s);
-            while(st.hasMoreTokens()){
-                double x=0;
-                try{x=Double.parseDouble(st.nextToken());} catch(NumberFormatException e){System.out.println("problem when loading choose_vector, not a number");System.out.println(x);}
+    public static void affectChooseVector(ChooseVector vector, BufferedReader reader) {
+        int i = 0;
+        String s = "";
+        try {
+            s = reader.readLine();
+            s = reader.readLine();// System.out.println(s);
+            StringTokenizer st = new StringTokenizer(s);
+            while (st.hasMoreTokens()) {
+                double x = 0;
+                try {
+                    x = Double.parseDouble(st.nextToken());
+                } catch (NumberFormatException e) {
+                    System.out.println("problem when loading choose_vector, not a number");
+                    System.out.println(x);
+                }
                 vector.setDoubleAt(i++, x);
             }
-        }
-        catch(IOException e) {System.out.println(e);}// we pass the line indicating the beginning of the vector
+        } catch (IOException e) {
+            System.out.println(e);
+        }// we pass the line indicating the beginning of the vector
 
     }
 
-    public static void printVectors(){
+    public static void findSize(String size, BufferedReader reader) {
+        String toCompare = "case" + size; // System.out.println(toCompare);
+        String s = "";
+        while (true) {
+            try {
+                s = reader.readLine();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+            if ((s != null) && (s.compareTo(toCompare) == 0)) {
+                return;
+            }
+        }
+    }
+
+    public static void printVectors() {
         BinaryTree.ch_1_or_u.toScreen();
         BinaryTree.ch_1_or_v.toScreen();
         BinaryTree.ch_u_or_v.toScreen();
