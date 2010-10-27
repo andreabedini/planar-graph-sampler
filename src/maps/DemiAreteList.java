5d3f4e1b2c1d9e3610d5f0aff43d500dc3951eb6
Andrea Bedini <andrea.bedini@gmail.com>
2010-10-27 11:37:02 +1100

    Reformat

--- src/maps/DemiAreteList.java
+++ src/maps/DemiAreteList.java
@@ -6,23 +6,22 @@ import java.util.ArrayList;
  *
  * @author fusy
  */
-public class DemiAreteList extends ArrayList{
-    
+public class DemiAreteList extends ArrayList {
+
     /**
-     * Creates a new instance of DemiAreteList 
+     * Creates a new instance of DemiAreteList
      */
     public DemiAreteList() {
     }
-    
-    public DemiArete getDemiArete(int i){
-        return (DemiArete)(this.get(i));
+
+    public DemiArete getDemiArete(int i) {
+        return (DemiArete) (this.get(i));
     }
-    
-    public void toScreen(){
-        for (int i=0;i<this.size();++i) {
-            DemiArete demiArete=(DemiArete)this.get(i);
-            System.out.print(demiArete.index+" ");
+
+    public void toScreen() {
+        for (int i = 0; i < this.size(); ++i) {
+            DemiArete demiArete = (DemiArete) this.get(i);
+            System.out.print(demiArete.index + " ");
         }
     }
-    
 }
