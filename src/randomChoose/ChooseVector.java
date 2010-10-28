0db753bbc123f9dbc7828cfc48c3121583b0f03d
Andrea Bedini <abedini@ms.unimelb.edu.au>
2010-10-28 15:46:54 +1100

    Reformat

--- src/randomChoose/ChooseVector.java
+++ src/randomChoose/ChooseVector.java
@@ -2,43 +2,49 @@ package randomChoose;
 
 import java.util.Random;
 
-public class ChooseVector{
+public class ChooseVector {
     // each ChooseVector has an array of values summing to 1, which corresponds to a Bernoulli-choice
+
     double[] values;
-    int nr_values=0;
-    
+    int nr_values = 0;
+
     /** Creates a new instance of ChooseVector */
     public ChooseVector(int n) {
-        values=new double[n-1];
-        this.nr_values=n-1;
+        values = new double[n - 1];
+        this.nr_values = n - 1;
     }
-    
-    public double getDoubleAt(int i){
+
+    public double getDoubleAt(int i) {
         return this.values[i];
     }
-    
-    public void setDoubleAt(int i, double x){
-        if(i>=this.nr_values) System.out.println(x);
-        this.values[i]=x;
+
+    public void setDoubleAt(int i, double x) {
+        if (i >= this.nr_values) {
+            System.out.println(x);
+        }
+        this.values[i] = x;
     }
-    
+
     // if the array values is [p0,p1,...,pk], the procedure choose returns a random index i in [0,i-1]
     // such that Pr(i)=pi
-    public int choose(Random random){
-        int i=0; 
-        double u=random.nextDouble();
+    public int choose(Random random) {
+        int i = 0;
+        double u = random.nextDouble();
         while (true) {
-            if (u<=this.values[i]) return i;
-            i++; 
-            if(i>=this.nr_values) return this.nr_values;
+            if (u <= this.values[i]) {
+                return i;
+            }
+            i++;
+            if (i >= this.nr_values) {
+                return this.nr_values;
+            }
         }
     }
-    
-    public void toScreen(){
-        for (int i=0;i<nr_values;i++){
-            System.out.println("value at index "+i+": "+this.getDoubleAt(i));
+
+    public void toScreen() {
+        for (int i = 0; i < nr_values; i++) {
+            System.out.println("value at index " + i + ": " + this.getDoubleAt(i));
         }
         System.out.println("");
     }
-    
 }
