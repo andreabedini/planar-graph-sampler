37c96db90a6aec3dff3e6c90473ff4db4854da91
Andrea Bedini <andrea@andreabedini.com>
2014-10-20 17:57:30 +1100

    Better exception handling

--- driver.py
+++ driver.py
@@ -22,41 +22,43 @@ with shelve.open('status.shelve', writeback=True) as status, open(outfilename, '
     number_of_runs = status.get('number_runs', 0)
     total_generated = status.get('total_generated', 0)
 
-    cmdline = 'java -jar {}.jar {}'.format(program, chunk_size)
-    
-    target_reached = False
-    while not target_reached:
-        try:
-            result = subprocess.check_output(shlex.split(cmdline))
-            number_of_runs = number_of_runs + 1
-        except subprocess.CalledProcessError:
-            pass
+    try:
+        cmdline = 'java -jar {}.jar {}'.format(program, chunk_size)
 
-        lines = result.splitlines()
-        total_generated = total_generated + len(lines)
+        target_reached = False
+        while not target_reached:
+            try:
+                result = subprocess.check_output(shlex.split(cmdline))
+                number_of_runs = number_of_runs + 1
+            except subprocess.CalledProcessError:
+                pass
 
-        for line in lines:
-            line = line.decode()
-            n, e, graph = line.split()
-            n = int(n)
-            if n < size_min or n > size_max:
-                continue
-            
-            out.write(line + '\n')
-            histogram[n] = histogram.get(n, 0) + 1
-        
-        total_kept = sum(histogram.values())
+            lines = result.splitlines()
+            total_generated = total_generated + len(lines)
 
-        target_reached = True
-        for value in histogram.values():
-            if value < target:
-                target_reached = False
-                break
-        
-        print("Total kept ", total_kept)
-        print("Total generated", total_generated)
-        print("Minimum value", min(histogram.values()))
-        
-    status['histogram'] = histogram
-    status['number_runs'] = number_of_runs
-    status['total_generated'] = total_generated
+            for line in lines:
+                line = line.decode()
+                n, e, graph = line.split()
+                n = int(n)
+                if n < size_min or n > size_max:
+                    continue
+
+                out.write(line + '\n')
+                histogram[n] = histogram.get(n, 0) + 1
+
+            total_kept = sum(histogram.values())
+
+            target_reached = True
+            for value in histogram.values():
+                if value < target:
+                    target_reached = False
+                    break
+
+            print("Total kept ", total_kept)
+            print("Total generated", total_generated)
+            print("Minimum value", min(histogram.values()))
+    finally:
+        status['histogram'] = histogram
+        status['number_runs'] = number_of_runs
+        status['total_generated'] = total_generated
+
