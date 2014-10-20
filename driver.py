dec26b95b84e70d13d9c6a5966493ec85df92097
Andrea Bedini <andrea@andreabedini.com>
2014-10-20 17:47:36 +1100

    Blah

--- driver.py
+++ driver.py
@@ -3,32 +3,36 @@
 import shlex
 import subprocess
 import shelve
+import sys
 
 size_min = 50
 size_max = 200
 target = 100
 chunk_size = 1000
 
-program = 'TwoConnected'
+if len(sys.argv) < 2:
+    print('you forgot the program')
+    sys.exit()
+
+program = sys.argv[1]
 outfilename = program + '.out'
 
 with shelve.open('status.shelve', writeback=True) as status, open(outfilename, 'a') as out:
     histogram = status.get('histogram', {})
     number_of_runs = status.get('number_runs', 0)
     total_generated = status.get('total_generated', 0)
-    total_kept = status.get('total_kept', 0)
 
     cmdline = 'java -jar {}.jar {}'.format(program, chunk_size)
     
     target_reached = False
     while not target_reached:
         try:
-            out = subprocess.check_output(shlex.split(cmdline))
+            result = subprocess.check_output(shlex.split(cmdline))
             number_of_runs = number_of_runs + 1
         except subprocess.CalledProcessError:
             pass
 
-        lines = out.splitlines()
+        lines = result.splitlines()
         total_generated = total_generated + len(lines)
 
         for line in lines:
@@ -56,4 +60,3 @@ with shelve.open('status.shelve', writeback=True) as status, open(outfilename, '
     status['histogram'] = histogram
     status['number_runs'] = number_of_runs
     status['total_generated'] = total_generated
-    status['total_kept'] = total_kept
